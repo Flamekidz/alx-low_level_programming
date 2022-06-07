7ddcf1dda364f1f7317dde03f045acb3ee674e24
Flamekidz <briansegem296@gmail.com>
2022-06-07 04:39:48 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,3 +7,11 @@ puts("\"Programming is like building a multilingual puzzle);
 return (0)
 }
  
+ #include <stdio.h>
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle);
+return (0)
+}
+ 
