ae27f926d098bc43e74a054496d53a58da8c43f9
Flamekidz <briansegem296@gmail.com>
2022-06-09 03:29:08 -0700

    printf5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,15 @@
-# include < stdio.h >
+# include <stdio.h>
 
 /**
- * main - Entry Point 
- *  Description - displays with proper grammar, but the outcome is a piece of art,
- *  Return : 0
+ *  main - Entry Point 
+ *   Description : displays the text 
+ *    Return : 0
  */
 
 int main (void)
 
 {
-	printf("with proper grammar, but the outcome is a piece of art,"\n);
-
-0
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return 0;
 }
 
