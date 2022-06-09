bb7787fc2f3b1f6d86fc78333790abef55a379cd
Flamekidz <briansegem296@gmail.com>
2022-06-08 21:41:35 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-nclude <stdio.h>
 
+# include <stdio.h>
 /**
- * main - Entry Point
+ *  main - Entry Point 
+ *   Description : puts statement 
+ *   Return : 0 Always (Success)
  *
- * Return : Always 0 (Success)
- * /
- int main(void)
- {
- puts("\"Programming is like building a multilingual puzzle);
- return (0)
- }
-
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0)
+}
 
