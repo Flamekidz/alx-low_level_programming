eaaa4c354e3ef2eda15118249239e2b975b7d994
Flamekidz <briansegem296@gmail.com>
2022-06-09 05:04:05 -0700

    puts4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-
 # include <stdio.h>
+
 /**
- *  main - Entry Point 
- *   Description : puts statement 
- *   Return : 0 Always (Success)
- *
+ * main - Starts the program 
+ * description: displays text 
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
-	return (0)
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
-
