37df9d2b570eb2bd4b242261ffb395478b6d5545
Flamekidz <briansegem296@gmail.com>
2022-06-12 15:13:36 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: prints all base 10 numbers less than 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+
+		printf("%d", n);
+	    printf("\n");
+
+	    return (0);
+}
