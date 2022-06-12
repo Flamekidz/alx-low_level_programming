01cb70da3c9bcd708cc4fcfe61a53c96f8d8e76f
Flamekidz <briansegem296@gmail.com>
2022-06-12 13:55:54 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry Point
- * Description: display characters
+ * Description: print alphaBET
  * Return: Always 0 (Success)
  */
 
@@ -11,10 +11,13 @@ int main(void)
 	char check;
 
 	for (check = 'a'; check <= 'z'; check++)
-		for (check = 'A'; check <= 'Z'; check++)
-		{
-			putchar(check);
-		}
+	{
+		putchar(check);
+	}
+	for (check = 'A'; check <= 'Z'; check++)
+	{
+		putchar(check);
+	}
 	putchar('\n');
 	return (0);
 }
