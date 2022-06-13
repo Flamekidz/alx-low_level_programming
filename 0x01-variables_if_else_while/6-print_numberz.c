99d4195fa354f043e41e63e0addfe67998694fa5
Flamekidz <briansegem296@gmail.com>
2022-06-12 21:33:44 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: displays base 10 numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 27; n < 37; n++)
+	{
+		putchar('\n');
+	}
+	putchar('\n');
+	return (0);
+}
