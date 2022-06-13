5066278927836f943c192ce08d3e62acd65ec294
Flamekidz <briansegem296@gmail.com>
2022-06-13 14:47:15 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: displays combinations less hundred 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, z;
+
+	for (x = 0; x < 100; x++)
+	{
+		for (z = 0; z < 100; z++)
+
+		{
+			if (x < z)
+				{
+					putchar((x / 10) + 48);
+					putchar((x % 10) + 48);
+					putchar(' ');
+					putchar((z / 10) + 48);
+					putchar((z % 10) + 48);
+					if (x != 98 || z != 99)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
