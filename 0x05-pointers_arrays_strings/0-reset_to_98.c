122cd0e12f570201db8c2cb3f40f03d2242ba737
Flamekidz <briansegem296@gmail.com>
2022-06-13 22:37:19 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 function taking pointer as parameter
+ * Description:98 taken as argument
+ * Return: nothing 
+ */
+void reset_to_98(int *j)
+{
+	*j = 98;
+
