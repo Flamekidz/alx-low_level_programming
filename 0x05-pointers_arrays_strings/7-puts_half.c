11fe453fac331c50dc56607a913e21585d096672
Flamekidz <briansegem296@gmail.com>
2022-06-14 18:32:46 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_diagonal - entry point
+ * @n: string output
+ * Return: null
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int x, y;
+
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < n; y++}
+					{
+					if(y == x)
+					{
+					_putchar('\\');
+					}
+					else if (y < x)
+					{
+					_putchar(' ');
+					}
+					_putchar('\n');
+					}
+					}
+					}
+					}
