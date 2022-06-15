1ff048cc408c04babaeb6f71e07443b94f54df73
Flamekidz <briansegem296@gmail.com>
2022-06-15 07:31:56 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Entry Point
+ * main - Entry point 
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	returns (0);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,5 @@
 0x02-functions_nested_loops
+0x02-functions_nested_loops
+0x02-functions_nested_loops
+0x02-functions_nested_loops
+0x02-functions_nested_loops
