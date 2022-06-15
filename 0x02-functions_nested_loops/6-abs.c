9872c8a86788d42fc9b84b98fbd2546dc80f1e4f
Flamekidz <briansegem296@gmail.com>
2022-06-15 13:16:52 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - Entry Point
+ * @n: character inspected
+ * Return: n if true else -n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
