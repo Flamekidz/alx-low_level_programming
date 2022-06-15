fe7f9ec2701e5326e04943a2a2b18a01c18522e5
Flamekidz <briansegem296@gmail.com>
2022-06-15 14:37:30 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry Point
+ * @n: inspected character
+ * int: integer
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+	{
+		n = -n;
+		a = n % 10;
+		if (a < 0)
+		{
+			a = -a;
+		}
+		_putchar(a + '0');
+	}
+	return (a);
+}
