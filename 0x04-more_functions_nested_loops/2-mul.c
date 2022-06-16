e7050cbc22f8c24c237c767aabe8c0936977cff4
Flamekidz <briansegem296@gmail.com>
2022-06-15 20:41:24 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * @a: first integer
+ * @b: second integer
+ * return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
