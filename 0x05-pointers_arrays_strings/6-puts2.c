463597eca0df07c3bb3fcf575b5a588a62443b66
Flamekidz <briansegem296@gmail.com>
2022-06-17 01:51:40 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,19 @@
 #include "main.h"
 
 /**
- * puts2 - function that prints strings in even reversal
- * @s: string output
- *
- * Return: null
+ * puts2 - a function that prints every other character of a string
+ * starting with the first character, followed by a new line
+ * @str: An input string
+ * Return: Nothing
  */
 
-void puts2(char *s)
+void puts2(char *str)
 {
-	int j=0;
-
-	while (s[j])
-	{
-		if (j % 2 == 0)
-		{
-			_putchar(s[j]);
-			j++;
-		}
-		_putchar('\n');
-	}
+	int len = 0, i = 0;
+	while (str[len] != '\0')
+		len++;
+	len -= 1;
+	for (; i <= len; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
 }
