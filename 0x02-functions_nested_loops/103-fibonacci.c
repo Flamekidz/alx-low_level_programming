b3d6cccd63ba79df97ec79827a0cae964ff55fa6
Flamekidz <briansegem296@gmail.com>
2022-06-16 23:48:15 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,7 +2,7 @@
 
 /**
  * main - main block
- * Description: computes and prints even  number < 4,000,000
+ * Description: computes and prints the sum of all the multiples of 3 or
  * 5 below 1024 (excluded), followed by a new line
  * Return: 0
  */
@@ -10,20 +10,16 @@
 int main(void)
 {
 	int a = 0, b = 1, next = 0;
-
-	int sum =0;
+	int sum = 0;
 
 	while (next < 4000000)
 	{
 		next = a + b;
 		a = b;
 		b = next;
-
 		if (next % 2 == 0)
-		{
 			sum += next;
-		}
-		printf("%i\n", sum);
-		return (0);
 	}
+	printf("%i\n", sum);
+	return (0);
 }
